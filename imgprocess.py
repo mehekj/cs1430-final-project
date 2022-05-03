9f57fe81e0db5538fbc1d3e05d1ceb719d1271e8
mehekj <mehek.jethani@gmail.com>
2022-05-03 12:40:54 -0400

    intersection bug fix

--- imgprocess.py
+++ imgprocess.py
@@ -3,17 +3,27 @@ import cv2
 import numpy as np
 from skimage.color import rgb2gray
 
+'''
+handles the actual computer vision part
+'''
+
+
 def get_line_intersection(line1, line2):
-    rho1, theta1 = line1[0]
-    rho2, theta2 = line2[0]
+    rho1 = line1[0]
+    theta1 = line1[1]
+    rho2 = line2[0]
+    theta2 = line2[1]
     A = np.array([
         [np.cos(theta1), np.sin(theta1)],
         [np.cos(theta2), np.sin(theta2)]
     ])
     b = np.array([[rho1], [rho2]])
-    x0, y0 = np.linalg.solve(A, b)
-    x0, y0 = int(np.round(x0)), int(np.round(y0))
-    return [[x0, y0]]
+    if (np.linalg.det(A) != 0):
+        x0, y0 = np.linalg.solve(A, b)
+        x0, y0 = int(np.round(x0)), int(np.round(y0))
+        return [[x0, y0]]
+    return None
+
 
 def get_intersection_points(lines):
     intersections = []
@@ -21,12 +31,11 @@ def get_intersection_points(lines):
         for next_group in lines[i+1:]:
             for line1 in group:
                 for line2 in next_group:
-                    intersections.append(get_line_intersection(line1, line2)) 
+                    point = get_line_intersection(line1, line2)
+                    if point is not None:
+                        intersections.append(point)
     return intersections
 
-'''
-handles the actual computer vision part
-'''
 
 def get_lines(img):
     # convert to grayscale, blur, then get canny edges
@@ -42,12 +51,9 @@ def get_lines(img):
     xys = []
     if lines is not None:
         intersections = get_intersection_points(lines)
-        print(intersections)
-        for i in intersections:
-            x0 = intersections[0][0]
-            y0 = intersections[0][1]
-            cv2.circle(plot, (x0, y0), radius=2, color=(0, 0, 255), thickness=-1)
-
+        intersections = filter(lambda point : 
+            point[0][0] >= 0 and point[0][0] < plot.shape[1] and point[0][1] >= 0 and point[0][1] < plot.shape[0]
+        , intersections)
 
         for i in range(0, len(lines)):
             rho = lines[i][0][0]
@@ -60,4 +66,10 @@ def get_lines(img):
             pt2 = (int(x0 - 1000*(-b)), int(y0 - 1000*(a)))
             xys.append([pt1, pt2])
             cv2.line(plot, pt1, pt2, (0, 0, 255), 1, cv2.LINE_AA)
+
+        for i in list(intersections):
+            x0 = i[0][0]
+            y0 = i[0][1]
+            
+            cv2.circle(plot, (int(x0), int(y0)), radius=10, color=(255, 0, 0), thickness=-1)
     return plot
