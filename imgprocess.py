37bbee16a5ffccf637829b7aace6a01f57431e68
mehekj <mehek.jethani@gmail.com>
2022-05-07 14:33:48 -0400

    picks 18 best distinct lines

--- imgprocess.py
+++ imgprocess.py
@@ -3,6 +3,7 @@ import cv2
 import numpy as np
 from skimage.color import rgb2gray
 from sklearn.cluster import MiniBatchKMeans
+from sklearn.neighbors import NearestNeighbors
 
 DEGREE = np.pi/180
 
@@ -29,18 +30,19 @@ def get_line_intersection(line1, line2):
 
 
 # gets the intersection points of all the lines
-def get_intersection_points(horizontals, verticals):
+def get_intersection_points(lines):
     intersections = []
-    for horiz in horizontals:
-        for verts in verticals:
-            point = get_line_intersection(horiz, verts)
-            if point is not None:
-                intersections.append(point)
+    for line1 in lines:
+        for line2 in lines:
+            if not (line1[0] == line2[0] and line1[1] == line2[1]):
+                point = get_line_intersection(line1, line2)
+                if point is not None:
+                    intersections.append(point)
     return np.array(intersections)
 
 
 """classify lines as horizontal or vertical """
-def separate_lines(lines, threshold = DEGREE * 5):
+def separate_lines(lines, threshold = DEGREE * 20):
     horizontals = []
     verticals = []
     for line in lines:
@@ -52,8 +54,30 @@ def separate_lines(lines, threshold = DEGREE * 5):
     return np.array(horizontals + verticals), np.array(horizontals), np.array(verticals)
 
 
+def combine_lines(lines, rho_threshold=50, theta_threshold=np.pi / 3):
+    best_lines = np.zeros((18, 2))
+    count = 0
+    for i in range(lines.shape[0]):
+        rho = lines[i][0]
+        theta = lines[i][1]
+        if i == 0:
+            best_lines[count] = lines[i]
+            count += 1
+        else:
+            if rho < 0:
+                rho *= -1
+            closeness_rho = np.isclose(rho, best_lines[0:count, 0], atol=rho_threshold)
+            closeness_theta = np.isclose(theta, best_lines[0:count, 1], atol=theta_threshold)
+            closeness = np.all([closeness_rho, closeness_theta], axis=0)
+            if not any(closeness) and count < best_lines.shape[0]:
+                best_lines[count] = lines[i]
+                count += 1
+
+    return best_lines
+    
+
+
 def cluster_points(intersections):
-    # features = np.squeeze(np.array(intersections), axis=1)
     features = np.array(intersections)
     kmeans = MiniBatchKMeans(n_clusters=81, max_iter=500).fit(features)
     return np.ndarray.tolist(kmeans.cluster_centers_)
@@ -66,11 +90,12 @@ def get_lines(img):
     edges = cv2.Canny(img, 50, 200)
 
     # get hough transform lines
-    lines = cv2.HoughLines(edges, 1, np.pi / 180, 125, None)
+    lines = cv2.HoughLines(edges, 0.8, np.pi / 180, 125, None)
     return np.squeeze(lines, axis=1)
 
 
-def plot_lines(img, lines):
+def plot_lines(img, lines, color=(255, 0, 0)):
+    img = np.array(img)
     for i in range(0, len(lines)):
         rho = lines[i][0]
         theta = lines[i][1]
@@ -80,7 +105,7 @@ def plot_lines(img, lines):
         y0 = b * rho
         pt1 = (int(x0 + 1000*(-b)), int(y0 + 1000*(a)))
         pt2 = (int(x0 - 1000*(-b)), int(y0 - 1000*(a)))
-        cv2.line(img, pt1, pt2, (255, 0, 0), 1, cv2.LINE_AA)
+        cv2.line(img, pt1, pt2, color, 1, cv2.LINE_AA)
     
     return img
 
@@ -91,25 +116,19 @@ def plot_points(img, points):
         y0 = i[1]
         
         img = np.array(img)
-        cv2.circle(img, (int(x0), int(y0)), radius=10, color=(0, 255, 0), thickness=-1)
-        cv2.circle(img, (int(x0), int(y0)), radius=10, color=(0, 0, 255), thickness=2)
+        cv2.circle(img, (int(x0), int(y0)), radius=5, color=(0, 255, 0), thickness=-1)
+        cv2.circle(img, (int(x0), int(y0)), radius=5, color=(0, 0, 255), thickness=2)
     
     return img
 
 
-def get_board_corners(images):
-    all_intersections = []
-    for img in images:
-        lines = get_lines(img)
-        if lines is not None:
-            lines, horizontals, verticals = separate_lines(lines)
-            intersections = get_intersection_points(horizontals, verticals)
-            intersections = np.array(list(filter(lambda point : 
-                point[0] >= 0 and point[0] < img.shape[1] and point[1] >= 0 and point[1] < img.shape[0]
-                , intersections)))
-            all_intersections.append(intersections)
-    
-    nparray = np.concatenate(all_intersections, axis=0)
-    corners = cluster_points(nparray)
-    plot = plot_points(images[-1], corners)
-    return plot
+def get_board_corners(img):
+    lines = get_lines(img)
+    if lines is not None:
+        lines = combine_lines(lines)
+        intersections = get_intersection_points(lines)
+        intersections = np.array(list(filter(lambda point : point[0] >= 0 and point[0] < img.shape[1] and point[1] >= 0 and point[1] < img.shape[0], intersections)))
+        # corners = cluster_points(intersections)
+        plot = plot_points(plot_lines(img, lines), intersections)
+        return plot
+    return None
--- main.py
+++ main.py
@@ -15,15 +15,14 @@ handles IMG CAPTURE and USER CONTROLS
 vid = cv2.VideoCapture(0)
 last_cap = None
 edges = None
-num_caps = 10
-captured_frames = []
-cap_count = -1
   
 while(True):
-    # read live video feed
-    _, frame = vid.read()
-    # frame = np.flip(io.imread('test.jpeg'), axis=-1) # google image
-    # frame = np.flip(io.imread('savetest.jpg'), axis=-1) # our board
+    # _, frame = vid.read() # read live video feed
+    frame = np.flip(io.imread('savetest.jpg'), axis=-1) # our board
+
+    if frame is None:
+        print("no camera input")
+        break
 
     # crop to square
     if frame.shape[1] > frame.shape[0]:
@@ -48,21 +47,11 @@ while(True):
     if key == ord('q'):
         break
     # captures photo and gets processed plot
-    elif key == ord(' ') and cap_count == -1:
-        # last_cap = imgprocess.get_lines(frame)
-        captured_frames = []
-        cap_count = 0
+    elif key == ord(' '):
+        last_cap = imgprocess.get_board_corners(frame)
     # save img
     elif key == ord('s'):
         cv2.imwrite('savetest.jpg', cv2.resize(frame, (720, 720)))
-    
-    if cap_count != -1 and cap_count < num_caps:
-        captured_frames.append(frame)
-        cap_count += 1
-        if cap_count == num_caps:
-            last_cap = imgprocess.get_board_corners(captured_frames)
-            captured_frames = []
-            cap_count = -1
 
 # quit program release cap obj and destroy windows
 vid.release()
