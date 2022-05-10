b6f1436165e51cf6bd3bc8596cc5539af961ded1
eatthepear <thelordofpear@gmail.com>
2022-05-09 21:25:51 -0400

    added comments to gamelogic

--- game.py
+++ game.py
@@ -42,6 +42,9 @@ piece_chars = {
 }
 
 class Board:
+    '''
+    Initializes the board.
+    '''
     def __init__(self) -> None:
         self.board =  np.array([[Piece.BR, Piece.BN, Piece.BB, Piece.BQ, Piece.BK, Piece.BB, Piece.BN, Piece.BR],
                                 [Piece.BP, Piece.BP, Piece.BP, Piece.BP, Piece.BP, Piece.BP, Piece.BP, Piece.BP],
@@ -52,8 +55,8 @@ class Board:
                                 [Piece.WP, Piece.WP, Piece.WP, Piece.WP, Piece.WP, Piece.WP, Piece.WP, Piece.WP],
                                 [Piece.WR, Piece.WN, Piece.WB, Piece.WQ, Piece.WK, Piece.WB, Piece.WN, Piece.WR]],
                                 dtype=Piece)
-        self.filled_board = np.array([[2, 2, 2, 2, 2, 2, 2, 2],
-                                      [2, 2, 2, 2, 2, 2, 2, 2],
+        self.filled_board = np.array([[1, 1, 1, 1, 1, 1, 1, 1],
+                                      [1, 1, 1, 1, 1, 1, 1, 1],
                                       [0, 0, 0, 0, 0, 0, 0, 0],
                                       [0, 0, 0, 0, 0, 0, 0, 0],
                                       [0, 0, 0, 0, 0, 0, 0, 0],
@@ -63,12 +66,19 @@ class Board:
         self.turn = Turn.WHITE
         self.verify_board()
 
-    def get_fen_for_new_state(self, input_board):
-        self.calculate_difference(input_board)
+    '''
+    Called by imgprocess.py.
+    input_board - a 2d array of 1's and 0's that represents what the camera sees as filled in or not
+    '''
+    def get_fen_for_new_state(self, input_board, captured_x, captured_y):
+        self.calculate_difference(input_board, captured_x, captured_y)
         self.verify_board()
         return self.board_to_fen()
 
-    def calculate_difference(self, input_filled_or_not):
+    '''
+    calculates the difference between the old board and the new board
+    '''
+    def calculate_difference(self, input_filled_or_not, captured_x, captured_y):
         has_piece_been_taken = np.sum(self.filled_board) - np.sum(input_filled_or_not)
         if has_piece_been_taken==0:
             # no piece taken
@@ -85,14 +95,11 @@ class Board:
                         y_old = j
         else:
             # piece taken
-            find_taken_piece_board = input_filled_or_not + self.filled_board
+            x_new = captured_x
+            y_new = captured_y
             differences = input_filled_or_not - self.filled_board
             for i in range(8):
                 for j in range(8):
-                    # only if it used to be a piece, and then it's a different color piece, so we have 1 + 2 = 3
-                    if find_taken_piece_board[i][j] == 3:
-                        x_new = i
-                        y_new = j
                     if differences[i][j] < 0:
                         # If the old square was filled but now is empty, difference is -1 or -2
                         x_old = i
@@ -100,17 +107,16 @@ class Board:
         # Run updates
         self.update_after_turn(input_filled_or_not, x_new, y_new, x_old, y_old)
 
-# WQ 2, 2
-# BR 3, 3
-# old filled board at 2,2 is 1 (WHITE)
-# at 3,3 is 2 (black)
-# new input at 2,2 is 0
-# new input at 3,3 is 1
-
+    '''
+    Updates the pieces on the board.
+    '''
     def update_pieces(self, x_new, y_new, x_old, y_old):
         self.board[x_new, y_new] = self.board[x_old, y_old]
         self.board[x_old, y_old] = Piece.E
 
+    '''
+    Updates all of the fields.
+    '''
     def update_after_turn(self, new_filled_board, x_new, y_new, x_old, y_old):
         self.filled_board = new_filled_board
         self.update_pieces(x_new, y_new, x_old, y_old)
@@ -119,9 +125,15 @@ class Board:
         else:
             self.turn = Turn.WHITE
 
+    '''
+    Prints the board for testing purposes.
+    '''
     def print_board(self):
         print(self.board)
 
+    '''
+    Verifies that self.filled_board represents the same board as self.board.
+    '''
     def verify_board(self):
         for i in range(8):
             for j in range(8):
@@ -131,12 +143,15 @@ class Board:
                 elif current_piece==Piece.WK or current_piece==Piece.WQ or current_piece==Piece.WR or current_piece==Piece.WB or current_piece==Piece.WN or current_piece==Piece.WP:
                     Assert(self.filled_board[i][j]==1)
                 elif current_piece==Piece.BK or current_piece==Piece.BQ or current_piece==Piece.BR or current_piece==Piece.BB or current_piece==Piece.BN or current_piece==Piece.BP:
-                    Assert(self.filled_board[i][j]==2)
+                    Assert(self.filled_board[i][j]==1)
         print("Board seems good!")
         self.print_board()
 
 # https://stackoverflow.com/questions/56754543/generate-chess-board-diagram-from-an-array-of-positions-in-python
 
+    '''
+    Makes the board into an FEN for display purposes.
+    '''
     def board_to_fen(self):
         # Use StringIO to build string more efficiently than concatenating
         with io.StringIO() as s:
